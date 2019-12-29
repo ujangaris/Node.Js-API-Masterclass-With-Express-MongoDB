df37de546ffba6d36bd63477522daa1f81fb9c20
ujangaja <ujangaja@gmail.com>
2019-12-30 06:21:49 +0700

    Creating Routers & Response in Express

--- server.js
+++ server.js
@@ -9,5 +9,40 @@ dotenv.config({
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Show all bootcamps`
+    })
+})
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Show bootcamp ${req.params.id}`
+    })
+})
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Create new bootcamp`
+    })
+})
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Update bootcamp ${req.params.id}`
+    })
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Delete bootcamp ${req.params.id}`
+    })
+})
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log(`Server runing in ${process.env.NODE_ENV} mode on port ${PORT}`));
