ec438d88237f2b1f1d6f0e02fcc38eba69a67537
ujangaja <ujangaja@gmail.com>
2019-12-30 06:28:59 +0700

    Using The express Router

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+
+// Route file
+const bootcamps = require('./routes/bootcamps')
+
+
 //Load env vars
 dotenv.config({
     path: './config/config.env'
@@ -9,40 +14,9 @@ dotenv.config({
 
 const app = express();
 
-app.get('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `Show all bootcamps`
-    })
-})
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps)
 
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `Show bootcamp ${req.params.id}`
-    })
-})
-
-app.post('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `Create new bootcamp`
-    })
-})
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `Update bootcamp ${req.params.id}`
-    })
-})
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `Delete bootcamp ${req.params.id}`
-    })
-})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log(`Server runing in ${process.env.NODE_ENV} mode on port ${PORT}`));
