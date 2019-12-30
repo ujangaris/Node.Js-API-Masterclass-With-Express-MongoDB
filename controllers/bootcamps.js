771f1d65b6e27594490957113e23c669f2739173
ujangaja <ujangaja@gmail.com>
2019-12-30 12:39:29 +0700

    delete bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -93,8 +93,21 @@ exports.updateBootcamp = async (req, res, next) => {
 //@route    DELETE /api/v1/bootcamps/:id
 //@access   Private
 exports.deleteBootcamp = async (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: `Delete bootcamp ${req.params.id}`
-    })
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
+        if (!bootcamp) {
+            return res.status(400).json({
+                success: false
+            })
+
+        }
+        res.status(200).json({
+            success: true,
+            data: {}
+        })
+    } catch (err) {
+        res.status(400).json({
+            success: false
+        })
+    }
 }
