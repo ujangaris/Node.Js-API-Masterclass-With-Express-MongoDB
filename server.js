e018717066ffb5ffb0114c0456f3de0364a60f69
ujangaja <ujangaja@gmail.com>
2019-12-30 07:10:02 +0700

    intro to middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger')
 
 
 // Route file
@@ -13,6 +14,8 @@ dotenv.config({
 
 
 const app = express();
+
+app.use(logger);
 
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps)
