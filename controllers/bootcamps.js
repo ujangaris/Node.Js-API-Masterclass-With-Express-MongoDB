9c91c5e0e3504fd1bfbbb4836b26d5220d6e8d44
ujangaja <ujangaja@gmail.com>
2019-12-30 11:58:45 +0700

    fetching data all bootcamps and single bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,21 +2,38 @@ const Bootcamp = require('../models/Bootcamp')
 //@desc     Get all bootcamps
 //@route    GET /api/v1/bootcamps
 //@access   Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: `Show all bootcamps`
-    })
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.find();
+        res.status(200).json({
+            success: true,
+            data: bootcamp
+        })
+    } catch (err) {
+        res.status(400).json({
+            success: false
+        })
+    }
 }
 
 //@desc     Get single bootcamps
 //@route    GET /api/v1/bootcamp/:id
 //@access   Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: `Show bootcamp ${req.params.id}`
-    })
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        res.status(200).json({
+            success: true,
+            data: bootcamp
+        })
+    } catch (err) {
+        res.status(400).json({
+            success: false
+        })
+    }
+
+
 }
 
 //@desc     Create new bootcamps
